3cb3b1aa00c40c19028021dbac547ec2a8dc63ce
Shubh27129 <shubham27129@gmail.com>
2023-08-18 14:29:32 +0530

    Routers and Filters

--- app.js
+++ app.js
@@ -1,41 +1,17 @@
-// const http= require('http')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const express= require('express');
+const app = express();
 
-const bodyParser= require('body-parser')
-
-const app= express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use('/admin',adminRoutes);
+app.use('/shop', shopRoutes);
 
-
-// app.use((req, res, next)=>{
-//     console.log('In the Middleware')
-//     next()                          //Allows the request to continue to next middleware in line
-// })
-
-
-app.use('/add-product',(req, res, next)=>{
-    // console.log('In product page Middleware')
-    res.send('<form action="/product" method="POST"><label>Name of Product: </label><input type="text" name="title"><label>Size of Product: </label><input type="text" name="size"><button type="submit">Add Product</button></form>')
-    
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
-
-app.post('/product',(req, res, next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/',(req, res, next)=>{
-    // console.log('In the another Middleware')
-    res.send('<h1>Express js Tutorial</h1>')
-    
-})
-
-
-// const server = http.createServer(app);
-
-// server.listen(3002);
-
 app.listen(3000);
